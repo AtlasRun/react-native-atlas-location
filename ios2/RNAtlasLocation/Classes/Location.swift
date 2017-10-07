9bd2f027304e6ab6d9acab37f7db79f5fff4a8c3
seo <seotownsend@icloud.com>
2017-10-07 14:44:47 -0700

    Moved -> ios2 for submodule issues

---
+++ ios2/RNAtlasLocation/Classes/Location.swift
@@ -0,0 +1,5 @@
+class Location {
+  func hello() -> String {
+    return "Hello World"
+  }
+}
---
+++ ios2/RNAtlasLocation/Example/Pods/Target Support Files/RNAtlasLocation/RNAtlasLocation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RNAtlasLocation : NSObject
+@end
+@implementation PodsDummy_RNAtlasLocation
+@end
